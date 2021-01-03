1b13143762fdc42c22a324442e39a30faa4b111c
Trevor Stubbs <stubbste@gmail.com>
2021-01-03 13:01:52 -0800

    This is my initial commit with 2  files that includes my name and start time

---
+++ LightningLawInterviewRound1/StartTime.cs
@@ -0,0 +1,5 @@
+﻿/*  Applicant and Start Time
+    Name: Trevor Stubbs
+    Github Repo: https://github.com/TrevorStubbs/Restaurant-Manager
+    Start Time: 1:00pm 
+*/
---
+++ Starttime.md
@@ -0,0 +1,4 @@
+# Applicant and Start Time
+- Name: Trevor Stubbs
+- Github Repo: https://github.com/TrevorStubbs/Restaurant-Manager
+- Start Time: 1:00pm 
